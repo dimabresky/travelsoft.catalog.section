57fa0013835005ae5241f540d0159a272233677f
Дмитрий <dmitrij@iMac-Dmitrij.local>
2016-09-26 22:46:10 +0300

    add catalog.section

---
+++ travelsoft.catalog.section/lang/ru/.description.php
@@ -0,0 +1,4 @@
+<?
+$MESS ['IBLOCK_SECTION_TEMPLATE_NAME'] = "Элементы раздела модифицированный";
+$MESS ['IBLOCK_SECTION_TEMPLATE_DESCRIPTION'] = "Выводит элементы раздела с указанным набором свойств, цен и т.д.";
+?>
